4e0008bf5f05e0f5c063f29ca2cdbb27a2281f4c
karanssidhu96 <karanssidhu96@gmail.com>
2018-03-12 16:41:36 +0000

    Simplified windowing process and now have data and tests for 25hz

--- run.py
+++ run.py
@@ -3,53 +3,53 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report, precision_score
 from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
-from numpy import reshape, nanmean, where, isnan, take
+from numpy import reshape
 from timeit import default_timer as timer
 
 NO_OF_FILES = 65
+NO_OF_FEATURES = 9
+GENUINE_LABEL = 1
+FRAUDULENT_LABEL = 0
+NO_ROWS_TRAINING_100_HZ = 162000
+NO_ROWS_TESTING_100_HZ = 54000
+ROWS_PER_WINDOW_100_HZ_5_SECONDS = 500
+ROWS_PER_WINDOW_100_HZ_1_SECOND = 100
+NO_ROWS_TRAINING_25_HZ = 40500
+NO_ROWS_TESTING_25_HZ = 13500
+ROWS_PER_WINDOW_25_HZ_5_SECONDS = 125
+ROWS_PER_WINDOW_25_HZ_1_SECOND = 25
 
-#For 5 second windows -> 162000 windows of 500 rows -> 1st half genuine & second half fraudulent
-def create_training_window(training_data):
+
+#For 5 second windows -> 162000 rows total, 500 rows per window -> 1st half genuine & second half fraudulent
+def create_window(data, no_rows_per_file, no_rows_per_window):
     windows = []
-    for i in range(0,81000,500):
+    for i in range(0,int(no_rows_per_file/2),no_rows_per_window):
         window = []
-        for j in range(9):
-            window.append(training_data.iloc[i:i+500, j].values)
-        window.append(1)
+        for j in range(NO_OF_FEATURES):
+            window.append(data.iloc[i:i + no_rows_per_window, j].values)
+        window.append(GENUINE_LABEL)
         windows.append(window)
 
-    for i in range(81000,162000,500):
+    for i in range(int(no_rows_per_file/2), no_rows_per_file,no_rows_per_window):
         window = []
-        for j in range(9):
-            window.append(training_data.iloc[i:i + 500, j].values)
-        window.append(0)
+        for j in range(NO_OF_FEATURES):
+            window.append(data.iloc[i:i + no_rows_per_window, j].values)
+        window.append(FRAUDULENT_LABEL)
         windows.append(window)
     return windows
 
-#For 5 second windows => 54000 windows of 500 rows -> 1st half genuine & second half fraudulent
-def create_testing_window(testing_data):
-    windows = []
-    for i in range(0,27000, 500):
-        window = []
-        for j in range(9):
-            window.append(testing_data.iloc[i:i+500, j].values)
-        window.append(1)
-        windows.append(window)
 
-    for i in range(27000,54000,500):
-        window = []
-        for j in range(9):
-            window.append(testing_data.iloc[i:i + 500, j].values)
-        window.append(0)
-        windows.append(window)
-    return windows
-
-def load_experiment(start, end, experiment):
+def load_experiment(start, end, experiment, frequency):
     for i in range(start, end):
-        training_files.append('Supervised_100_Hz/train_activity_experiment_f100_acc_exp' + str(experiment) + '_' + str(i) + '.csv')
-        testing_files.append('Supervised_100_Hz/test_activity_experiment_f100_acc_exp' + str(experiment) + '_' + str(i) + '.csv')
+        training_files.append('Supervised_'+ str(frequency) +'_Hz/train_activity_experiment_f'+ str(frequency) +'_acc_exp' + str(experiment) + '_' + str(i) + '.csv')
+        testing_files.append('Supervised_'+ str(frequency) +'_Hz/test_activity_experiment_f'+ str(frequency) +'_acc_exp' + str(experiment) + '_' + str(i) + '.csv')
 
-def load_data():
+def load_experiment_25_Hz(start, end, experiment):
+    for i in range(start, end):
+        training_files.append('Supervised_25_Hz/train_activity_experiment_f25_acc_exp' + str(experiment) + '_' + str(i) + '.csv')
+        testing_files.append('Supervised_25_Hz/test_activity_experiment_f25_acc_exp' + str(experiment) + '_' + str(i) + '.csv')
+
+def load_data(frequency):
     print('Loading data')
     #start = 10
     #end = 21
@@ -57,12 +57,12 @@ def load_data():
         #load_experiment(start, end, experiment)
         #start = start + 10
         #end = end + 10
-    load_experiment(10, 21, 1)
-    load_experiment(20, 31, 2)
-    load_experiment(30, 40, 3)
-    load_experiment(40, 51, 4)
-    load_experiment(50, 61, 5)
-    load_experiment(60, 71, 6)
+    load_experiment(10, 21, 1, frequency)
+    load_experiment(20, 31, 2, frequency)
+    load_experiment(30, 40, 3, frequency)
+    load_experiment(40, 51, 4, frequency)
+    load_experiment(50, 61, 5, frequency)
+    load_experiment(60, 71, 6, frequency)
 
 def seperate_data_from_labels(window, x, y):
     x_internal = []
@@ -98,26 +98,36 @@ if __name__== '__main__':
 
     min_max_scaler = preprocessing.MinMaxScaler()
     linear_scaler_to_unit_variance = preprocessing.StandardScaler()
-    load_data()
+    # 100 Hz
+    #load_data(100)
+    # 25 Hz
+    load_data(25)
 
     print('Second Loop')
     for i in range(NO_OF_FILES):
         current_train_file = pd.read_csv(training_files[i])
         current_test_file = pd.read_csv(testing_files[i])
         print('Scaling and windowing user:', i)
-        #Fails on experiment 3 34
+        #Fails on experiment 3 34 both frequencies
 
-        #current_train_file = scaler_training(min_max_scaler, current_train_file)
-        #current_test_file = scaler_testing(min_max_scaler, current_test_file)
+        current_train_file = scaler_training(min_max_scaler, current_train_file)
+        current_test_file = scaler_testing(min_max_scaler, current_test_file)
 
-        current_train_file = scaler_training(linear_scaler_to_unit_variance, current_train_file)
-        current_test_file = scaler_testing(linear_scaler_to_unit_variance, current_test_file)
+        #current_train_file = scaler_training(linear_scaler_to_unit_variance, current_train_file)
+        #current_test_file = scaler_testing(linear_scaler_to_unit_variance, current_test_file)
 
         training_data.append(current_train_file)
         testing_data.append(current_test_file)
 
-        training_window = create_training_window(training_data[i])
-        testing_window = create_testing_window(testing_data[i])
+        # 5 second window 100 Hz
+        #training_window = create_window(training_data[i], NO_ROWS_TRAINING_100_HZ, ROWS_PER_WINDOW_100_HZ_5_SECONDS)
+        #testing_window = create_window(testing_data[i], NO_ROWS_TESTING_100_HZ, ROWS_PER_WINDOW_100_HZ_5_SECONDS)
+        # 5 second window 25 Hz
+        training_window = create_window(training_data[i], NO_ROWS_TRAINING_25_HZ, ROWS_PER_WINDOW_25_HZ_5_SECONDS)
+        testing_window = create_window(testing_data[i], NO_ROWS_TESTING_25_HZ, ROWS_PER_WINDOW_25_HZ_5_SECONDS)
+        # 1 second window 100 Hz
+        #training_window = create_window(training_data[i], NO_ROWS_TRAINING_100_HZ, ROWS_PER_WINDOW_100_HZ_1_SECOND)
+        #testing_window = create_window(testing_data[i], NO_ROWS_TESTING_100_HZ, ROWS_PER_WINDOW_100_HZ_1_SECOND)
 
         seperate_data_from_labels(training_window, x, y)
         seperate_data_from_labels(testing_window, test_x, test_y)
@@ -128,13 +138,23 @@ if __name__== '__main__':
     for i in range(NO_OF_FILES):
         print('Training User',i+1)
         start_time = timer()
-        train_user(classifier, x[i], y[i], 324, 4500)
+        #5 Seconds Training 100 Hz
+        #train_user(classifier, x[i], y[i], 324, 4500)
+        # 5 Seconds Training 25 Hz
+        train_user(classifier, x[i], y[i], 324, 1125)
+        #1 Second Training 100 Hz
+        #train_user(classifier, x[i], y[i], 1620, 900)
         end_time = timer()
         training_time = end_time - start_time
         print('Time to train user',i+1, ':', training_time)
         print('Testing User ',i+1)
         start_time = timer()
-        predictions = test_user(classifier, test_x[i], 108, 4500)
+        # 5 Seconds Testing 100 Hz
+        #predictions = test_user(classifier, test_x[i], 108, 4500)
+        # 5 Seconds Testing 25
+        predictions = test_user(classifier, test_x[i], 108, 1125)
+        # 1 Seconds Testing 100 Hz
+        #predictions = test_user(classifier, test_x[i], 540, 900)
         end_time = timer()
         testing_time = end_time - start_time
         print('Time to test user', i + 1, ':', testing_time)
@@ -147,7 +167,7 @@ if __name__== '__main__':
     #print(confusion)
     #print(classification_report(actual, predictions))
     #print(precision_score(actual, predictions))
-    precision = precision/NO_OF_FILES
-    avg_train_time = avg_train_time/NO_OF_FILES
-    avg_test_time = avg_test_time/NO_OF_FILES
+    precision = precision / NO_OF_FILES
+    avg_train_time = avg_train_time / NO_OF_FILES
+    avg_test_time = avg_test_time / NO_OF_FILES
     print('Accuracy:', precision, "\nAverage Training Time Per User:", avg_train_time, "\nAverage Testing Time Per User:", avg_test_time)
